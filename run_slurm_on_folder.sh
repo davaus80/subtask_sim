15fdff3bf3c9d696b1cdd9a7e92a515cc274fa6b
David <aviday80@gmail.com>
2025-11-04 21:39:00 -0500

    slurm folder

---
+++ run_slurm_on_folder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=sky
+#SBATCH --output=slurm_outputs/%x_%j.out
+#SBATCH --error=slurm_outputs/%x_%j.err
+#SBATCH --gres=gpu:rtx8000:4
+#SBATCH --time=10:00:00
+#SBATCH --mem=30G
+
+# Pass config as an argument
+CONFIG_DIR=$1
+
+module load python/3.10
+source ~/skyfall310/bin/activate
+
+export HF_HOME=$SCRATCH/hf_cache
+
+python experiment_manager.py --config_dir $CONFIG_DIR
