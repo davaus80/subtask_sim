3f1e58106add30ec5c983721718f4526f1394e9f
David <aviday80@gmail.com>
2026-03-25 11:08:33 -0400

    results

--- update_all_missing_logs.sh
+++ update_all_missing_logs.sh
@@ -2,25 +2,25 @@
 
 # List of folder names to process
 folders=(
-    # "experiments/20260120_ab_novar"
-    # "experiments/20260124_abandit_2sd_var"
-    # "experiments/20260124_abandit_4sd_var"
-    # "experiments/20260118_farm_novar"
-    # "experiments/20260124_farm_2sd_var"
-    # "experiments/20260124_farm_4sd_var"
-    # "experiments/20260312_rec_novar"
-    # "experiments/20260312_rec_4sd_var"
-    # "experiments/20260312_rec_2sd_var"
-    # "experiments/20260311_gemini_farm_novar"
-    # "experiments/20260312_gemini_ab_novar"
-    "experiments/20260320_ab_scalesweep_novar"
-    "experiments/20260320_farm_scalesweep_novar"
-    "experiments/20260320_clothing_scalesweep_novar"
+    "experiments/20260120_ab_novar"
+    "experiments/20260124_abandit_2sd_var"
+    "experiments/20260124_abandit_4sd_var"
+    "experiments/20260118_farm_novar"
+    "experiments/20260124_farm_2sd_var"
+    "experiments/20260124_farm_4sd_var"
+    "experiments/20260312_rec_novar"
+    "experiments/20260312_rec_4sd_var"
+    "experiments/20260312_rec_2sd_var"
+    "experiments/20260311_gemini_farm_novar"
+    "experiments/20260312_gemini_ab_novar"
+    # "experiments/20260320_ab_scalesweep_novar"
+    # "experiments/20260320_farm_scalesweep_novar"
+    # "experiments/20260320_clothing_scalesweep_novar"
 )
 
 # Loop through each folder and execute the script
 for folder in "${folders[@]}"; do
     echo "Processing $folder"
-    # ./find_incomplete_runs.sh "$folder"
-    ./find_incomplete_scalesweep_runs.sh "$folder"
+    ./find_incomplete_runs.sh "$folder"
+    # ./find_incomplete_scalesweep_runs.sh "$folder"
 done
